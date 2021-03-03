ea6c085d54e15e313ac3971f590ea3a79f764475
Graham Hall <ghall89@me.com>
2021-03-02 21:00:49 -0500

    move results to an array instead of rendering them on the fly

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,8 @@ function getData(searchKey, searchValue, searchKey2, searchValue2) {
 			if (response.ok) {
 				response.json()
 					.then(function(data) {
+						let resultsArr = [];
+						
 						// iterate through each villager object
 						for (let i = 0; i < 391; i++) {
 							let villagerKey = Object.keys(data)[i];
@@ -21,16 +23,17 @@ function getData(searchKey, searchValue, searchKey2, searchValue2) {
 							let villager = data[villagerKey]
 							if (searchKey2 && searchValue2) {
 								if (villager[searchKey] == searchValue && villager[searchKey2] == searchValue2) {
-									displayInfo(villager);
+									resultsArr.push(villager);
 								}
 							} else {
 								if (villager[searchKey] == searchValue) {
-									displayInfo(villager);
+									resultsArr.push(villager);
 								}
 							}
 
 						}
-
+						console.log(resultsArr);
+						displayInfo(resultsArr);
 					});
 			} else {
 				alert("Error, could not connect to ACNH API");
@@ -39,57 +42,61 @@ function getData(searchKey, searchValue, searchKey2, searchValue2) {
 
 }
 
-function displayInfo(villager) {
+function displayInfo(resultsArr) {
+	
+	for (let i = 0; i < resultsArr.length; i++) {
+		// create Bootstrap elements for each villager
+	
+		// Bootstrap columns
+		columnEl = document.createElement("div");
+		columnEl.classList = "col-12 col-md-6 col-lg-4 gy-3"
+	
+		// Bootstrap cards
+		const cardEl = document.createElement("div");
+		cardEl.className = "card";
+		cardEl.setAttribute("style", "width: 100%");
+		const imgEl = document.createElement("img");
+		imgEl.setAttribute("src", resultsArr[i].image_uri);
+		imgEl.className = "card-img-top";
+		const bodyEl = document.createElement("div")
+		bodyEl.className = "card-body"
+	
+		//card contents
+		// name
+		const nameEl = document.createElement("h5");
+		nameEl.className = "card-title";
+		nameEl.textContent = resultsArr[i].name["name-USen"];
+		// descriptor
+		const personalityEl = document.createElement("p");
+		personalityEl.classList = "card-text text-secondary";
+		personalityEl.textContent = resultsArr[i].personality + " Villager";
+		// quote
+		const sayingEl = document.createElement("p");
+		sayingEl.className = "card-text";
+		sayingEl.textContent = '"' + resultsArr[i].saying + '"';
+		// birthday
+		const listEl = document.createElement("ul");
+		listEl.classList = "list-group list-group-flush";
+		const birthdayEl = document.createElement("li");
+		birthdayEl.className = "list-group-item";
+		birthdayEl.textContent = "Birthday: " + resultsArr[i]["birthday-string"];
+	
+		//display elements
+		infoEl.appendChild(columnEl)
+	
+		columnEl.appendChild(cardEl);
+		cardEl.appendChild(imgEl);
+		cardEl.appendChild(bodyEl);
+	
+		bodyEl.appendChild(nameEl);
+		bodyEl.appendChild(personalityEl);
+		bodyEl.appendChild(sayingEl);
+	
+		cardEl.appendChild(listEl);
+		listEl.appendChild(birthdayEl);
+}
 
-	// create Bootstrap elements for each villager
-
-	// Bootstrap columns
-	columnEl = document.createElement("div");
-	columnEl.classList = "col-12 col-md-6 col-lg-4 gy-3"
-
-	// Bootstrap cards
-	const cardEl = document.createElement("div");
-	cardEl.className = "card";
-	cardEl.setAttribute("style", "width: 100%");
-	const imgEl = document.createElement("img");
-	imgEl.setAttribute("src", villager.image_uri);
-	imgEl.className = "card-img-top";
-	const bodyEl = document.createElement("div")
-	bodyEl.className = "card-body"
-
-	//card contents
-	// villager name
-	const nameEl = document.createElement("h5");
-	nameEl.className = "card-title";
-	nameEl.textContent = villager.name["name-USen"];
-	// descriptor
-	const personalityEl = document.createElement("p");
-	personalityEl.classList = "card-text text-secondary";
-	personalityEl.textContent = villager.personality + " Villager";
-	// quote
-	const sayingEl = document.createElement("p");
-	sayingEl.className = "card-text";
-	sayingEl.textContent = '"' + villager.saying + '"';
-	// birthday
-	const listEl = document.createElement("ul");
-	listEl.classList = "list-group list-group-flush";
-	const birthdayEl = document.createElement("li");
-	birthdayEl.className = "list-group-item";
-	birthdayEl.textContent = "Birthday: " + villager["birthday-string"];
-
-	//display elements
-	infoEl.appendChild(columnEl)
-
-	columnEl.appendChild(cardEl);
-	cardEl.appendChild(imgEl);
-	cardEl.appendChild(bodyEl);
-
-	bodyEl.appendChild(nameEl);
-	bodyEl.appendChild(personalityEl);
-	bodyEl.appendChild(sayingEl);
-
-	cardEl.appendChild(listEl);
-	listEl.appendChild(birthdayEl);
+	
 }
 
 
